9ebe0200ddcb8783c554a7b84fbf456355dae120
Terukula Sai <codesai127.0.0.1@gmail.com>
2025-01-04 18:59:43 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test {
     {
         int a=50;
         int b=189;
-        int c=8000;
+        int c=800;
         int res=a+b+c;
         System.out.println("the result is " +res);
     }
